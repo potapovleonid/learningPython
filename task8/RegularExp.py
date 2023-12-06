927057179cd4b1af0068de23d3c2db6a0ab06f7c
Leonid <kissmykrabeskill@gmail.com>
2023-12-06 22:14:09 +0500

    Task 8 +regex check price

--- task8/RegularExp.py
+++ task8/RegularExp.py
@@ -29,4 +29,25 @@ print(f'Likes: {likes}, Reposts: {reposts}')
 tweet_23 = 'Какое замечательное место! Обязательно вернусть сюда снова. Всем советую!!!'
 print(re.split(r'[!?.]+\s', tweet_23))
 
-#1:35
+'''
+
+'''
+passwords = ['Apple34!rose', 'My87hou#4$', 'abc123']
+res = []
+for i in passwords:
+    # if re.findall(r'[*#$%!&.][a-zA-Z][0-9]', i) and 8 <= len(i) <= 20:
+    if re.findall(r'[\w*#$%!&.]{8,20}', i):
+        res.append(i)
+
+print(res)
+
+report = 'файл 11.txt загружен, файл 22.txt загружен, файл 33.txt ошибка'
+
+file_download = r'\d+\.txt(?=\sзагружен)'
+file_error = r'\d+\.txt(?!\sзагружен)'
+print(re.findall(file_download, report))
+print(re.findall(file_error, report))
+
+prices = 'RUB4.44, RUB10.88, EUR0.99, RUB99.99'
+list_prices = re.findall(r'(?<=RUB)[0-9.]+', prices)
+print(sum(float(i) for i in list_prices))
