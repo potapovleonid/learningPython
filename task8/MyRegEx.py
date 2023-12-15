158896b8c5cbfa35caaa6dd33ddd0e66e9a425fa
Leonid <kissmykrabeskill@gmail.com>
2023-12-16 01:10:59 +0500

    Task 8 check email valid

--- task8/MyRegEx.py
+++ task8/MyRegEx.py
@@ -5,10 +5,12 @@ pattern_auto_number = r'^[A-Z]{1}\d{3}[A-Z]{2}\d{2,3}$'
 
 def check_valid_auto_number(number: str):
     result = re.findall(pattern_auto_number, number)
-    return {result[0][0:6]: result[0][6:]} if result else False
+    return [
+        {'Number': result[0][0:6]},
+        {'Region': result[0][6:]}] if result else f"Number: {number} isn't valid"
 
 
-def check_valid():
+def check_valid_auto_numbers():
     print(check_valid_auto_number('A555AA1'))
     print(check_valid_auto_number('B555BB22'))
     print(check_valid_auto_number('B555BB333'))
@@ -21,4 +23,21 @@ def check_valid():
     print(check_valid_auto_number('Ф555ЫЫ'))
 
 
-check_valid()
+pattern_email = r'^[\w-]{4,}@([\w-]+\.)[\w]+$'
+
+
+def check_valid_email(email: str):
+    return f'Email {email} is {True}' if re.findall(pattern_email, email) else f'Email {email} is {False}'
+
+
+def check_valid_emails():
+    print(check_valid_email('potapo!v-lv@mail.ru'))
+    print(check_valid_email('pota*pov-lv@mail.ru'))
+    print(check_valid_email('potapo!v-lv@mail.ru'))
+    print(check_valid_email('pota$po!v-lv@mail.ru'))
+    print(check_valid_email('potapolv-lv@mail.ru'))
+    print(check_valid_email('potap%o!v-lv@mail.ru'))
+
+
+check_valid_auto_numbers()
+check_valid_emails()
