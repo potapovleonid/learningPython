aae4db5a63d4fb705b8af5750076077e2d60f98d
Leonid <kissmykrabeskill@gmail.com>
2023-12-22 23:55:37 +0500

    Task 9 encapsulation

--- task9/MyWallet.py
+++ task9/MyWallet.py
@@ -1,5 +1,13 @@
+def move_money(out_wallet, in_wallet, value):
+    if out_wallet is Wallet and in_wallet is Wallet:
+        out_wallet.top_down_money(value)
+        in_wallet.add_money(value)
+
+
 class Wallet:
     def __init__(self, currency, money: int = 0):
+        if currency not in ('USD', 'RUB', 'EURO'):
+            raise ValueError(f'ERROR type currency {currency}')
         self.__currency = currency
         self.__money = money
 
@@ -13,11 +21,19 @@ class Wallet:
         self.__money -= value
         print(self.info())
 
+    def move_money(self, in_wallet, value):
+        if type(in_wallet) is Wallet:
+            self.top_down_money(value)
+            in_wallet.add_money(value)
+
     def info(self):
         return f'You have {self.__money} {self.__currency}'
 
 
 w1 = Wallet('USD', 50)
+w2 = Wallet('USD', 50)
 w1.top_down_money(50)
 w1.add_money(500)
 w1.top_down_money(50)
+print('_')
+w1.move_money(w2, 100)
