b60db6488ad741cc246251ef25407dc226836bd2
Leonid <kissmykrabeskill@gmail.com>
2023-12-23 00:00:29 +0500

    Task 9 encapsulation

--- task9/MyWallet.py
+++ task9/MyWallet.py
@@ -5,11 +5,12 @@ def move_money(out_wallet, in_wallet, value):
 
 
 class Wallet:
-    def __init__(self, currency, money: int = 0):
+    def __init__(self, currency, owner, money: int = 0):
         if currency not in ('USD', 'RUB', 'EURO'):
             raise ValueError(f'ERROR type currency {currency}')
         self.__currency = currency
         self.__money = money
+        self.__owner = owner
 
     def add_money(self, value: int):
         self.__money += value
@@ -24,16 +25,19 @@ class Wallet:
     def move_money(self, in_wallet, value):
         if type(in_wallet) is Wallet:
             self.top_down_money(value)
+            print(f'{self.__owner} moves transaction to {in_wallet.get_owner()} by summ {value}')
             in_wallet.add_money(value)
 
     def info(self):
-        return f'You have {self.__money} {self.__currency}'
+        return f'{self.__owner} has {self.__money} {self.__currency}'
+
+    def get_owner(self):
+        return self.__owner
 
 
-w1 = Wallet('USD', 50)
-w2 = Wallet('USD', 50)
+w1 = Wallet('USD', 'Jimm', 50, )
+w2 = Wallet('USD', 'Helena', 50, )
 w1.top_down_money(50)
 w1.add_money(500)
 w1.top_down_money(50)
-print('_')
 w1.move_money(w2, 100)
